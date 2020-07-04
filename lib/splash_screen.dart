abcc463500b8e4998949bf66fbd9e1f68d85d53b
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2020-07-04 21:42:24 +0530

    pretty printed

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -15,21 +15,22 @@ class _SplashScreenState extends State<SplashScreen>
 
   Widget logo({Color backgroundColor: Colors.white}) {
     return Center(
-        child: AnimatedContainer(
-            child: CircleAvatar(
-                backgroundImage: AssetImage('assets/logo/512x512.png'),
-                radius: 100,
-                backgroundColor: backgroundColor),
-            height: 200,
-            width: 200,
-            decoration: BoxDecoration(shape: BoxShape.circle, boxShadow: [
-              new BoxShadow(
-                  color: Color.fromRGBO(0, 0, 0, 0.16),
-                  blurRadius: 6.0,
-                  offset: const Offset(0, 3))
-            ]),
-            duration: Duration(seconds: 1),
-            curve: Curves.fastOutSlowIn));
+      child: AnimatedContainer(
+          child: CircleAvatar(
+              backgroundImage: AssetImage('assets/logo/512x512.png'),
+              radius: 100,
+              backgroundColor: backgroundColor),
+          height: 200,
+          width: 200,
+          decoration: BoxDecoration(shape: BoxShape.circle, boxShadow: [
+            new BoxShadow(
+                color: Color.fromRGBO(0, 0, 0, 0.16),
+                blurRadius: 6.0,
+                offset: const Offset(0, 3))
+          ]),
+          duration: Duration(seconds: 1),
+          curve: Curves.fastOutSlowIn),
+    );
   }
 
   var _color = Colors.white;
@@ -93,22 +94,23 @@ class _SplashScreenState extends State<SplashScreen>
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Stack(children: <Widget>[
-      //
-      // Background Container (Pink)
+      child: Stack(
+        children: <Widget>[
+          //
+          // Background Container (Pink)
 
-      AnimatedContainer(
-          decoration: BoxDecoration(color: _color),
-          duration: Duration(seconds: 1),
-          curve: Curves.fastOutSlowIn),
-      //
-      // Circular Container (White)
+          AnimatedContainer(
+              decoration: BoxDecoration(color: _color),
+              duration: Duration(seconds: 1),
+              curve: Curves.fastOutSlowIn),
+          //
+          // Circular Container (White)
 
-      AnimatedPositioned(
-          top: _top,
-          left: -256 + (MediaQuery.of(context).size.width / 2),
-          child: Center(
-              child: Container(
+          AnimatedPositioned(
+              top: _top,
+              left: -256 + (MediaQuery.of(context).size.width / 2),
+              child: Center(
+                child: Container(
                   height: 512,
                   width: 512,
                   decoration: BoxDecoration(
@@ -116,23 +118,26 @@ class _SplashScreenState extends State<SplashScreen>
                       shape: BoxShape.circle,
                       boxShadow: [
                         _shadow,
-                      ]))),
-          duration: Duration(seconds: 1),
-          curve: Curves.fastOutSlowIn),
-      //
-      // Logo
+                      ]),
+                ),
+              ),
+              duration: Duration(seconds: 1),
+              curve: Curves.fastOutSlowIn),
+          //
+          // Logo
 
-      SlideTransition(
-          position: _offsetAnimation, child: logo(backgroundColor: _bgcolor)),
-      //
-      // Welcome Text
+          SlideTransition(
+              position: _offsetAnimation,
+              child: logo(backgroundColor: _bgcolor)),
+          //
+          // Secondary Widgets (Less Animated)
 
-      Positioned(
-          top: 280,
-          left: 32,
-          width: MediaQuery.of(context).size.width - 64,
-          child: Container(
-              child: Column(
+          Positioned(
+            top: 280,
+            left: 32,
+            width: MediaQuery.of(context).size.width - 64,
+            child: Container(
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     //
@@ -141,11 +146,13 @@ class _SplashScreenState extends State<SplashScreen>
                     AnimatedOpacity(
                         opacity: _wlcmOpacity,
                         duration: Duration(seconds: 1),
-                        child: Text("Welcome!",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 32,
-                                fontWeight: FontWeight.w500)),
+                        child: Text(
+                          "Welcome!",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 32,
+                              fontWeight: FontWeight.w500),
+                        ),
                         curve: Curves.fastOutSlowIn),
 
                     // Description Text
@@ -153,12 +160,14 @@ class _SplashScreenState extends State<SplashScreen>
                     AnimatedOpacity(
                         opacity: _descOpacity,
                         duration: Duration(seconds: 1),
-                        child: Text(descText,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.w400)),
+                        child: Text(
+                          descText,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w400),
+                        ),
                         curve: Curves.fastOutSlowIn),
 
                     // Join Us Button
@@ -167,33 +176,47 @@ class _SplashScreenState extends State<SplashScreen>
                         opacity: _bttnOpacity,
                         duration: Duration(seconds: 1),
                         child: RaisedButton(
-                            onPressed: () {
-                              // TODO: redirect to sign up/login screen
-                              print('redirect karo ise');
-                            },
-                            shape: StadiumBorder(),
-                            color: Colors.white,
-                            child: Padding(
-                                padding: EdgeInsets.fromLTRB(8, 4, 8, 8),
-                                child: const Text('Join Us',
-                                    style: TextStyle(
-                                        fontSize: 24,
-                                        fontWeight: FontWeight.w600,
-                                        color: Color(0xFF70307c))))),
+                          onPressed: () {
+                            // TODO: redirect to sign up/login screen
+                            print('redirect karo ise');
+                          },
+                          shape: StadiumBorder(),
+                          color: Colors.white,
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(8, 4, 8, 8),
+                            child: const Text(
+                              'Join Us',
+                              style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w600,
+                                  color: Color(0xFF70307c)),
+                            ),
+                          ),
+                        ),
                         curve: Curves.fastOutSlowIn),
+
+                    // Privacy Policy Link
+
                     AnimatedOpacity(
-                        opacity: _bttnOpacity,
-                        duration: Duration(seconds: 2),
-                        curve: Curves.easeInOut,
-                        child: new InkWell(
-                            child: Text('\n\nRead our privacy policy.',
-                                style: TextStyle(color: Colors.white)),
-                            onTap: () {
-                              // TODO: show privacy policy
-                              print('chull machi hai');
-                            })),
-                  ]),
-              alignment: Alignment.center)),
-    ]));
+                      opacity: _bttnOpacity,
+                      duration: Duration(seconds: 2),
+                      curve: Curves.easeInOut,
+                      child: new InkWell(
+                          child: Text('\n\nRead our privacy policy.',
+                              style: TextStyle(color: Colors.white)),
+                          onTap: () {
+                            // TODO: show privacy policy
+                            print('chull machi hai');
+                          }),
+                    ),
+
+                    //
+                  ],
+                ),
+                alignment: Alignment.center),
+          ),
+        ],
+      ),
+    );
   }
 }
